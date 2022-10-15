3a4f16b8ad3ec2be472f5526bcf7f7330930180e
YOUR NAME <jhanov@gmail.com>
2022-10-15 14:35:05 -0500

    subset laog

--- algorithms.rb
+++ algorithms.rb
@@ -979,36 +979,56 @@
 #   end
 # end
 
-def most_frequent_even(nums)
-  #     Loop through the array, if even -->
-  #     hash to store counters
-  #     Loop through the hash, find the ones with the highest count.
-  #     Go with the lowest if it's even.
+# def most_frequent_even(nums)
+#   #     Loop through the array, if even -->
+#   #     hash to store counters
+#   #     Loop through the hash, find the ones with the highest count.
+#   #     Go with the lowest if it's even.
 
-  even_counter = {}
-  nums.each do |num|
-    if num % 2 == 0
-      if even_counter[num] == nil
-        even_counter[num] = 0
-      end
-      even_counter[num] += 1
-    end
-  end
-  if even_counter == {}
-    return -1
-  end
-  highest_count = 0
-  even_counter.each do |k, v|
-    if v > highest_count
-      highest_count = v
-    end
-  end
-  highest = []
-  even_counter.each do |k, v|
-    if v == highest_count
-      highest << k
-    end
-  end
-  highest.sort!
-  highest[0]
-end
+#   even_counter = {}
+#   nums.each do |num|
+#     if num % 2 == 0
+#       if even_counter[num] == nil
+#         even_counter[num] = 0
+#       end
+#       even_counter[num] += 1
+#     end
+#   end
+#   if even_counter == {}
+#     return -1
+#   end
+#   highest_count = 0
+#   even_counter.each do |k, v|
+#     if v > highest_count
+#       highest_count = v
+#     end
+#   end
+#   highest = []
+#   even_counter.each do |k, v|
+#     if v == highest_count
+#       highest << k
+#     end
+#   end
+#   highest.sort!
+#   highest[0]
+# end
+
+# def find_subarrays(nums)
+#   #     Two Pointers x and y
+#   #     create a new subarray with x and y. find the sum.
+#   #     if the sum
+#       x = 0
+#       y = 1
+#       sums_hash = {}
+#       while y < nums.length
+#           sum = nums[x] + nums[y]
+#           p sum
+#           if sums_hash[sum]
+#               return true
+#           end
+#           sums_hash[sum] = "checked"
+#           x += 1
+#           y = x + 1
+#       end
+#       false
+#   end

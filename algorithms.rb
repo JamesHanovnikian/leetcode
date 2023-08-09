8e0e13d33b6949c23b569a361609bcd42ffbc1ff
YOUR NAME <jhanov@gmail.com>
2023-08-09 14:51:41 -0700

    longest uncommon subsequence

--- algorithms.rb
+++ algorithms.rb
@@ -3149,3 +3149,15 @@ end
 #       return false
 #   end
 # end
+
+# def find_lu_slength(a, b)
+#   if a == b
+#       return -1
+#   else
+#       if a.length > b.length
+#           return a.length
+#       else
+#           return b.length
+#       end
+#   end
+# end
